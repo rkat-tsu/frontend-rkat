7d1ef5b165f2f22ce8d86d11b1bab947b19cfc2f
KIJOTARO <aeidionagaia.werty@gmail.com>
2025-10-23 17:39:55 +0700

    Tambah BG

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,7 +3,18 @@ import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
+        <div
+            className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
+            style={{
+                backgroundImage: 'url("/img/backgrounds/login-bg.png")',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundColor: 'rgba(0,0,0,0.5)',
+                backgroundBlendMode: 'overlay',
+            }}
+        >
+
             <div>
                 <Link href="/">
                     <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
